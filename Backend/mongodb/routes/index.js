b8bbdfbab96a6c832c1c8cfb11205c142cd022df
nmachate <s0539778@htw-berlin.de>
2020-11-20 16:02:47 +0100

    backend auf git

---
+++ Backend/mongodb/routes/index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient( 'wka', 'JeKxj1OJ0nU8' );
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+var url = 'mongodb://wka:JeKxj1OJ0nU8@www.robert-magnus.de:27017/wka';
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+      assert.equal(null, err);
+    var cursor = db.collection('wka').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('wka').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+router.post('/update', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('wka').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item updated');
+      db.close();
+    });
+  });
+});
+
+router.post('/delete', function(req, res, next) {
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('wka').deleteOne({"_id": objectId(id)}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item deleted');
+      db.close();
+    });
+  });
+});
+
+module.exports = router;
